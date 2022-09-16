705e43d19b13372409fdd72a94f21fd5222aa239
Geronimo Anselmo <geronimoalejandroanselmo@gmail.com>
2022-09-16 00:20:56 -0300

    Completed Base News controller, routes, model, migration and seeders

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors')
-require('dotenv').config()
+const cors = require('cors');
+require('dotenv').config();
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const testimonialsRouter = require('./routes/testimonials');
 const categoriesRouter = require('./routes/categories');
+const newsRouter = require('./routes/news');
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/testimonials', testimonialsRouter);
 app.use('/categories', categoriesRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
