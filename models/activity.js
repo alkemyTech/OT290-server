52309ce970d3e138f05f8ec0f0ea8a2183f27b93
Nano204 <fgomeztenorio@hotmail.com>
2022-09-15 22:28:15 -0500

    Activity model and migration by sequelize-cli

--- models/activity.js
+++ models/activity.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Role extends Model {
+  class Activity extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,17 +11,18 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Role.init(
+  Activity.init(
     {
       name: DataTypes.STRING,
-      description: DataTypes.STRING,
+      image: DataTypes.STRING,
+      content: DataTypes.TEXT,
       deletedAt: DataTypes.DATE,
     },
     {
       sequelize,
-      modelName: "Role",
+      modelName: "Activity",
       paranoid: true,
     }
   );
-  return Role;
+  return Activity;
 };
