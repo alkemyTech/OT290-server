dbf3faf2503569915ce43bfe23394a3c4679c448
Nano204 <fgomeztenorio@hotmail.com>
2022-09-16 20:57:07 -0500

    Activities seeders created by sequelize-cli

--- seeders/20220917011043-create-activities.js
+++ seeders/20220917011043-create-activities.js
@@ -29,8 +29,8 @@ module.exports = {
             "https://www.designevo.com/res/templates/thumb_small/colorful-hand-and-warm-community.png",
           content:
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-          createdAt: new Date() - 1,
-          updatedAt: new Date() - 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
           deletedAt: new Date(),
         },
       ],
