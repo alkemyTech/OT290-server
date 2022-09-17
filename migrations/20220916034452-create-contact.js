2ccd17e543727c1e017e76bf843314148f9f26cd
Nano204 <fgomeztenorio@hotmail.com>
2022-09-16 19:33:19 -0500

    Comment model and migration created by sequelize-cli

--- migrations/20220916034452-create-contact.js
+++ migrations/20220916034452-create-contact.js
@@ -12,7 +12,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       phone: {
-        type: Sequelize.NUMBER
+        type: Sequelize.INTEGER
       },
       email: {
         type: Sequelize.STRING
