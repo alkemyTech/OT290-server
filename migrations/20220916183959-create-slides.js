6b1bb90b9742ddbae2f602bcfb0db9393ad26853
nahuelflores <arcangel6191@gmail.com>
2022-09-18 17:24:45 -0300

    Creado migracion para el modelo de Slides

---
+++ migrations/20220916183959-create-slides.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Slides', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      imageUrl: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      text: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      order: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Slides');
+  }
+};
