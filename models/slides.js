31fa3017414a67777fc9475c5794c3d0e08676fd
nahuelflores <arcangel6191@gmail.com>
2022-09-18 17:25:09 -0300

    Agregado de modelo de slides, relacionado al modelo organization.

---
+++ models/slides.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Slides extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Slides.belongsTo(models.organization,{
+        as: 'organizationId',
+      });
+    }
+  };
+  Slides.init({
+    imageUrl: DataTypes.STRING,
+    text: DataTypes.STRING,
+    order: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Slides',
+  });
+  return Slides;
+};
