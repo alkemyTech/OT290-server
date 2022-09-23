a46de0c86322a91e707445baab4b7f67de2e864d
nahuelflores <arcangel6191@gmail.com>
2022-09-23 14:26:46 -0300

    Agregado rutas basicas de CRUD de slides.

--- routes/slides.js
+++ routes/slides.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 const {
-  getSlides
+  getSlides,
+  getSlide,
+  createSlide,
+  updateSlide,
+  deleteSlide,
 } = require('../controllers/slides');
-
-router.get('/:id', getSlides);
+router.get('/', getSlides)
+router.get('/:id', getSlide);
+router.post('/', createSlide);
+router.put('/:id', updateSlide);
+router.delete('/:id', deleteSlide);
 
 module.exports = router;
