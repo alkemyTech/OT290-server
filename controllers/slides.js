85e6e62230d593a5b61d834f3e4a7723c7d71c42
Lucas Carnero <lucas.carnero@naranja.com>
2022-09-23 17:51:22 -0300

    fix in s3 configuration keys / fix in s3 file uploading logic

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,9 +1,10 @@
-const { Slides } = require('../models');
-const { Buffer } = require ('node:buffer');
+const { Slides } = require("../models");
+const { Buffer } = require("node:buffer");
+const base64 = require("base-64");
 
-const { uploadFileS3 } = require('../services/S3storage');
+const { uploadFileS3 } = require("../services/S3storage");
 
-const getSlides = async(req,res) =>{
+const getSlides = async (req, res) => {
   await Slides.findAll()
     .then((items) => {
       return res.status(200).json(items);
@@ -12,8 +13,7 @@ const getSlides = async(req,res) =>{
       console.log(err);
       return res.status(500).json(err);
     });
-
-}
+};
 
 const getSlide = async (req, res) => {
   const { id } = req.params;
@@ -31,21 +31,21 @@ const createSlide = async (req, res) => {
     const { image, text, organization_id } = req.body;
     let { order } = req.body;
     // BITMAP THE IMAGEN
-    let bitmap = Buffer.from(image).toString('base64');
-    let imageUrl = await uploadFileS3(bitmap);
-    if (!order){
+    let bitmap = Buffer.from(base64.decode(image)); // .toString('base64');
+    let imageUrl = await uploadFileS3(bitmap, "imagen.png");
+    if (!order) {
       order = await Slides.count({
         where: {
-          organizationId: organization_id
-        }
+          organizationId: organization_id,
+        },
       });
       order++;
-    };
+    }
     const newSlide = await Slides.create({
       imageUrl,
       text,
       order,
-      organizationId:organization_id
+      organizationId: organization_id,
     });
     newSlide.save();
     return res.status(201).json(newSlide);
@@ -55,44 +55,45 @@ const createSlide = async (req, res) => {
   }
 };
 
-const updateSlide = async (req,res) => {
+const updateSlide = async (req, res) => {
   try {
     const { id } = req.params;
     const { imageUrl, text, order, organization_id } = req.body;
     const slideFound = await Slides.findByPk(id);
     if (!slideFound) {
-      return res.status(404).send({'message':'Slide inexistente'});
+      return res.status(404).send({ message: "Slide inexistente" });
     }
-    await Slides.update({ imageUrl, text, order, organization_id }, {
-      where: {
-        id:id,
+    await Slides.update(
+      { imageUrl, text, order, organization_id },
+      {
+        where: {
+          id: id,
+        },
       }
-    });
-    return res.status(204).json(slideFound);
-  } catch (err) {
-    return res.status(500).json(err);
-  }
-}
-
-const deleteSlide = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const slideFound = await Slides.findByPk(id);
-    if (!slideFound) {
-      return res.status(404).send({'message':'Slide Inexistente'});
-    }
-    await Slides.destroy({
-      where: {
-        id:id,
-      }
-    });
+    );
     return res.status(204).json(slideFound);
   } catch (err) {
     return res.status(500).json(err);
   }
 };
 
-
+const deleteSlide = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const slideFound = await Slides.findByPk(id);
+    if (!slideFound) {
+      return res.status(404).send({ message: "Slide Inexistente" });
+    }
+    await Slides.destroy({
+      where: {
+        id: id,
+      },
+    });
+    return res.status(204).json(slideFound);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
 
 module.exports = {
   getSlides,
--- services/S3storage.js
+++ services/S3storage.js
@@ -1,38 +1,42 @@
-const AWS = require('aws-sdk');
-const fs = require('fs');
-require('dotenv').config();
+const AWS = require("aws-sdk");
+const fs = require("fs");
+require("dotenv").config();
 
 AWS.config.update({
-  AWS_ACCESS_KEY_ID: process.env.AWS_PUBLIC_KEY_ID,
-  AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_KEY,
-  AWS_REGION: process.env.AWS_REGION,
+  accessKeyId: process.env.AWS_PUBLIC_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: process.env.AWS_REGION,
 });
 
-const Bucket = process.env.BUCKET_NAME
-const S3 = require('aws-sdk/clients/s3');
+const Bucket = process.env.BUCKET_NAME;
+const S3 = require("aws-sdk/clients/s3");
 
 const s3 = new S3({
-  apiVersion: '2006-03-01'
+  apiVersion: "2006-03-01",
 });
 
-const uploadFileS3 = (fileName) => {
+// const uploadFileS3 = (fileName) => {
+const uploadFileS3 = (imageBuffer, imageName) => {
+  return new Promise((resolve, reject) => {
     // Setting up S3 upload parameters
-    const fileContent = fs.readFileSync(fileName);
+    // const fileContent = fs.readFileSync(fileName);
     const uploadParams = {
-        Bucket: Bucket,
-        Key: fileName,
-        Body: fileContent,
+      Bucket: Bucket,
+      Key: imageName,
+      Body: imageBuffer,
     };
 
     // Uploading files to the bucket
-    s3.upload(uploadParams, function(err, data) {
-        if (err) {
-            throw err;
-        }
-        return data.Location;
+    s3.upload(uploadParams, function (err, data) {
+      if (err) {
+        // throw err;
+        reject(err);
+      }
+      resolve(data.Location);
     });
+  });
 };
 
 module.exports = {
-  uploadFileS3
-  };
+  uploadFileS3,
+};
