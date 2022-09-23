a124cbccab51d79638fe1ab0cde32eb8695987bb
Baltasar Amaya <baltaamaya@gmail.com>
2022-09-23 18:37:34 -0300

    Middleswares separated in different files isOwnerorAdmin now only adds userId in req.params

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
+
+const isAdmin = async (req, res, next) => {
+    const tokenHeader = req.headers["authorization"];
+    try {
+        const token = tokenHeader.substring("Bearer ".length);
+        const {data} = jwt.verify(token, JWT_SECRET);
+        if (data.roleId === 1)
+        {
+            next()
+            return
+        }
+        return res.sendStatus(403);
+       
+    } catch (error) {
+        return res.status(401).json(error);
+    }
+};
+module.exports = { isAdmin };
