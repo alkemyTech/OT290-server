b3a2abd6afb3d0184ce31b6f4f96be1199fb91aa
nahuelflores <arcangel6191@gmail.com>
2022-09-23 14:22:05 -0300

    Agregado funciones basicas para el CRUD de slides. Modificiado funcion getSlide para que busque slides por su Id y no por organizacionesId ya que OT290-63 va en la parte public de organizacion.

--- controllers/slides.js
+++ controllers/slides.js
@@ -1,12 +1,10 @@
-const { Organization } = require('../models');
 const { Slides } = require('../models');
+const { Buffer } = require ('node:buffer');
 
-const getSlides = async (req, res) => {
-  const { id } = req.params;
-  await Slides.findAll({
-    where: { organizationId: id},
-    order: [['order','ASC']],
-  })
+const storage = require('../services/S3storage');
+
+const getSlides = async(req,res) =>{
+  await Slides.findAll()
     .then((items) => {
       return res.status(200).json(items);
     })
@@ -14,8 +12,82 @@ const getSlides = async (req, res) => {
       console.log(err);
       return res.status(500).json(err);
     });
+
+}
+
+const getSlide = async (req, res) => {
+  const { id } = req.params;
+  await Slides.findByPk(id)
+    .then((items) => {
+      return res.status(200).json(items);
+    })
+    .catch((err) => {
+      return res.status(500).json(err);
+    });
 };
+
+const createSlide = async (req, res) => {
+  try {
+    const { image, text, order, organization_id } = req.body;
+    // BITMAP THE IMAGEN
+    let bitmap = Buffer.from(image).toString('base64');
+    let imageUrl = await storage.uploadFileS3(bitmap);
+    const newSlide = await Slides.create({
+      imageUrl,
+      text,
+      order,
+      organization_id
+    });
+    newSlide.save();
+    return res.status(201).json(newSlide);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+const updateSlide = async (req,res) => {
+  try {
+    const { id } = req.params;
+    const { imageUrl, text, order, organization_id } = req.body;
+    const slideFound = await Slides.findByPk(id);
+    if (!slideFound) {
+      return res.status(404).send({'message':'Slide inexistente'});
+    }
+    await Slides.update({ imageUrl, text, order, organization_id }, {
+      where: {
+        id,
+      }
+    });
+    return res.status(204).json(slideFound);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+}
+
+const deleteSlide = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const slideFound = await Slides.findByPk(id);
+    if (!slideFound) {
+      return res.status(404).send({'message':'Slide Inexistente'});
+    }
+    await Slides.destroy({
+      where: {
+        id,
+      }
+    });
+    return res.status(204).json(slideFound);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+};
+
+
 
 module.exports = {
   getSlides,
+  getSlide,
+  createSlide,
+  updateSlide,
+  deleteSlide,
 };
