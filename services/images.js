42614cbc03303ca8971de67aa1be9c7519e97339
nahuelflores <arcangel6191@gmail.com>
2022-09-26 15:42:27 -0300

    Cambiado retorno de funcion decodeImage a un objeto para mejor accesibilidad.

--- services/images.js
+++ services/images.js
@@ -1,3 +1,6 @@
+const { Buffer } = require("node:buffer");
+const base64 = require("base-64");
+
 /**
  * 
  * @param {*} data String con formato "data:image/png;base64,iVBO"
@@ -5,8 +8,11 @@
 const decodeImage = (data) => {
   let general = data.split(';');
   let tipoImagen = general[0].split('/')[1];
-  let datos = general[1].split(',')[1];
-  return [tipoImagen,datos];
+  let datos = Buffer.from(base64.decode(data));
+  return {
+    'extension':tipoImagen,
+    'datos':datos
+  };
 }
 
 
