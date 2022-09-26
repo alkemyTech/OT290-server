5be4428c8c7a242655027783953b6c8a80dfc297
Lucas Carnero <lucas.carnero@naranja.com>
2022-09-26 17:13:18 -0300

    adds missing userData method in auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,10 +9,11 @@ const userRegister = async (req, res) => {
   try {
     //Nano: Validate errors in request to stop if there's any
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
     // Nano: Continue with user registry
     const user = await createUser(req, res);
-    
+
     if (user) {
       delete user.password;
       const token = signToken(user);
@@ -28,7 +29,8 @@ const userLogin = async (req, res) => {
     const { email, password } = req.body;
     const errors = validationResult(req);
 
-    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
     const user = await User.findOne({ where: { email } });
     const pass = user.password;
@@ -46,7 +48,24 @@ const userLogin = async (req, res) => {
   }
 };
 
+const userData = async (req, res) => {
+  try {
+    const tokenHeader = req.headers["Authorization"];
+    const token = tokenHeader.substring("Bearer ".length);
+    var decoded = jwt.verify(token, JWT_SECRET);
+    let id = decoded.id;
+    const user = await User.findById(id);
+    if (!user) {
+      return res.sendStatus(404);
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
 module.exports = {
   userRegister,
   userLogin,
+  userData,
 };
