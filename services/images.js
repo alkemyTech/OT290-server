91f27d39fb61efb2ea68b85553a91d4de854921d
Lucas Carnero <lucas.carnero@naranja.com>
2022-09-26 17:48:17 -0300

    adds improvements in image upload logic

--- services/images.js
+++ services/images.js
@@ -2,20 +2,20 @@ const { Buffer } = require("node:buffer");
 const base64 = require("base-64");
 
 /**
- * 
+ *
  * @param {*} data String con formato "data:image/png;base64,iVBO"
  */
 const decodeImage = (data) => {
-  let general = data.split(';');
-  let tipoImagen = general[0].split('/')[1];
-  let datos = Buffer.from(base64.decode(data));
+  let tipoMime, cadenaBase64;
+  [tipoMime, cadenaBase64] = data.split(";");
+  let extension = tipoMime.split("/")[1];
+  let datos = Buffer.from(base64.decode(cadenaBase64.substring(7)));
   return {
-    'extension':tipoImagen,
-    'datos':datos
+    extension,
+    datos,
   };
-}
+};
 
-
-module.export = {
+module.exports = {
   decodeImage,
-}
+};
