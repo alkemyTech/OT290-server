1693c5f9ae5e0216ed519bc86d898e7eebf992e9
Baltasar Amaya <baltaamaya@gmail.com>
2022-10-03 21:45:55 -0300

    Welcome email added with corresponding template

--- helpers/email.js
+++ helpers/email.js
@@ -1,24 +1,28 @@
 const client = require('@sendgrid/mail');
+const path = require('path');
+const ejs = require('ejs');
 
-const {
-  SENDGRID_API_KEY, SENDGRID_SENDER,
-} = process.env;
+const { SENDGRID_API_KEY, SENDGRID_SENDER } = process.env;
 
-const sendEmail = async (to) => {
-  client.setApiKey(SENDGRID_API_KEY);
-
-  const message = {
-    to,
-    from: SENDGRID_SENDER,
-    personalizations: [{
-      to,
-    }],
-  };
-  await client
-    .send(message)
-    .catch((error) => {
-      throw error;
-    });
+const sendEmail = async (to, firstName, lastName, facebook, linkedIn, instagram) => {
+  try {
+    client.setApiKey(SENDGRID_API_KEY);
+    let emailTemplate;
+    ejs.renderFile(path.join(__dirname, '/views/email-template.ejs'), 
+    { firstName, lastName, facebook, linkedIn, instagram })
+    .then(result => {
+      emailTemplate = result;
+      const message = {
+        to,
+        from: SENDGRID_SENDER,
+        subject: "Welcome link",
+        html: emailTemplate
+        };
+      return client.send(message)
+      })
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
-
+  
 module.exports = { sendEmail };
