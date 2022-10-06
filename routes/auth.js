c8994816e59bcd0b7c2c4a24aa2ed4cd6ab5caf1
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-05 21:32:13 -0300

    docs: adds swagger documentation for /auth/register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -6,9 +6,41 @@ const router = express.Router();
 // Nano: Import express validator to check types of input variables
 const { body } = require("express-validator");
 
-
 const { userRegister, userLogin, userData } = require("../controllers/auth");
 
+/**
+ * @openapi
+ * /auth/register:
+ *  post:
+ *     tags:
+ *     - Auth
+ *     description: Registrar un nuevo usuario
+ *     requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *             schema:
+ *                type: object
+ *                required:
+ *                  - firstName
+ *                  - lastNAme
+ *                  - email
+ *                  - password
+ *                properties:
+ *                  firstName:
+ *                    type: string
+ *                  lastName:
+ *                    type: string
+ *                  email:
+ *                    type: string
+ *                  password:
+ *                    type: string
+ *                  photo:
+ *                    type: string
+ *     responses:
+ *       201:
+ *         description: Usuario creado satisfactoriamente
+ */
 router.post(
   "/register",
   body("firstName").notEmpty(),
