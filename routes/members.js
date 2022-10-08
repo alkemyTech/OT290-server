932002441fb468358392c270b1ca88ce8010d930
Miguel Guerrero <yannokaiserfrom@gmail.com>
2022-10-08 08:47:18 -0300

    Corrigiendo formato de members

--- routes/members.js
+++ routes/members.js
@@ -1,23 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const { body } = require("express-validator");
-const { isAdmin }= require("../middlewares/isAdmin")
+const { isAdmin } = require("../middlewares/isAdmin");
 
 const {
   getMembers,
   getMember,
   createMember,
   updateMember,
-  deleteMember
-} = require('../controllers/members');
+  deleteMember,
+} = require("../controllers/members");
 
-router.get('/',isAdmin, getMembers);
-router.get('/:id', getMember);
-router.post('/', 
-                body("name").notEmpty().isString()
-                ,isAdmin
-                ,createMember);
-router.put('/:id', updateMember);
-router.delete('/:id',isAdmin, deleteMember);
+router.get("/", isAdmin, getMembers);
+router.get("/:id", getMember);
+router.post("/", body("name").notEmpty().isString(), isAdmin, createMember);
+router.put("/:id", updateMember);
+router.delete("/:id", isAdmin, deleteMember);
 
 module.exports = router;
