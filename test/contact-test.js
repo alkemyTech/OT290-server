71a409e682d16749e2e759ef2534507a8ccb3cf4
Miguel Guerrero <yannokaiserfrom@gmail.com>
2022-10-14 16:15:29 -0300

    Creado test para comprobacion en caso de no poseer credencailes

--- test/contact-test.js
+++ test/contact-test.js
@@ -7,61 +7,73 @@ const should = chai.should();
 const expect = require("chai").expect;
 const server = require("../app");
 let agent = chai.request.agent(server);
-//result
 
+let conseguirDatos = async () => {
+  //Consiguiendo el token necesario para validaciones
+  let get = await agent
+    .post("/auth/login")
+    .send({ email: "adminUser5@test.com", password: "##StrongPassword05" });
+  //Consiguiendo la respuesta del servidor para realizar validaciones
+  let getC = await agent
+    .get("/contacts")
+    .set("authorization", "Bearer " + get.body.accesToken);
+  //Consiguiendo la respuesta sin autenticacion
+  let getCv = await agent.get("/contacts");
+  return { getC, getCv };
+};
+
+//Descripcion de la suit de pruebas para contact
 describe("main suite", () => {
   describe("Caso 1 - GET contact", () => {
-    it("Comprueba que el status code sea correcto", async () => {
-      //consiguiendo la autorizacion
-      let get = await agent
-        .post("/auth/login")
-        .send({ email: "adminUser5@test.com", password: "##StrongPassword05" });
+    it("Comprueba el response de contact", async () => {
+      let datos = await conseguirDatos();
+      let getContact = datos.getC;
 
-      //consiguiendo los contactos
-      let getC = await agent
-        .get("/contacts")
-        .set("authorization", "Bearer " + get.body.accesToken);
-
-      expect(getC).to.have.status(200);
+      //Confirmaciones
+      expect(getContact).to.have.status(200);
+      getContact.body[0].should.be.a("object");
     });
-
-    describe("Caso 2 - GET contact", () => {
-      it("Validacion tipo de dato", async () => {
-        //consiguiendo la autorizacion
-        let get = await agent.post("/auth/login").send({
-          email: "adminUser5@test.com",
-          password: "##StrongPassword05",
-        });
-
-        //consiguiendo los contactos
-        let getC = await agent
-          .get("/contacts")
-          .set("authorization", "Bearer " + get.body.accesToken);
-
-        getC.body.forEach((contact) => {
-          assert.typeOf(
-            contact.id,
-            "number",
-            "El tipo de dato esperado es un string"
-          );
-          assert.typeOf(
-            contact.name,
-            "string",
-            "El tipo de dato esperado es un string"
-          );
-          assert.typeOf(
-            contact.phone,
-            "number",
-            "El tipo de dato esperado es un number"
-          );
-          assert.typeOf(
-            contact.email,
-            "string",
-            "El tipo de dato esperado es un string"
-          );
-          //assert.typeOf(contact.message, "text");
-        });
+  });
+  describe("Caso 2 - GET contact", () => {
+    it("Validacion tipo de dato", async () => {
+      let datos = await conseguirDatos();
+      let getContact = datos.getC;
+      getContact.body.forEach((contact) => {
+        assert.typeOf(
+          contact.id,
+          "number",
+          "El tipo de dato esperado es un string"
+        );
+        assert.typeOf(
+          contact.name,
+          "string",
+          "El tipo de dato esperado es un string"
+        );
+        assert.typeOf(
+          contact.phone,
+          "number",
+          "El tipo de dato esperado es un number"
+        );
+        assert.typeOf(
+          contact.email,
+          "string",
+          "El tipo de dato esperado es un string"
+        );
+        //assert.typeOf(contact.message, "text");
       });
+    });
+  });
+  describe("Caso 3 - GET contact sin autenticacion", () => {
+    it("Comprueba que sucede en caso de que no este autenticado", async () => {
+      let datos = await conseguirDatos();
+      let getContact = datos.getCv;
+      //Confirmaciones
+      expect(getContact).to.have.status(403);
+      assert.equal(
+        getContact.text,
+        "Forbidden",
+        "getContact.txt tiene que ser igual `Forbidden`"
+      );
     });
   });
 });
