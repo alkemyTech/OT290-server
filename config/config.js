0e2d6c870956f1a38367e57bdd717bc78ca19ecd
Miguel Guerrero <yannokaiserfrom@gmail.com>
2022-10-14 12:25:46 -0300

    creado test para la comprobacion de tipos en endoint

--- config/config.js
+++ config/config.js
@@ -1,26 +1,29 @@
-require('dotenv').config()
-
+require("dotenv").config();
+//se desactivo las consultas reflejadas en consola para visibilisar mejor los tes
 module.exports = {
-    "development": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database":  process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "port": process.env.DB_PORT,
-        "dialect": "mysql"
-    },
-    "test": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    },
-    "production": {
-        "username": process.env.DB_USER,
-        "password": process.env.DB_PASSWORD,
-        "database": process.env.DB_NAME,
-        "host": process.env.DB_HOST,
-        "dialect": "mysql"
-    }
-}
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  },
+};
--- test/contact-test.js
+++ test/contact-test.js
@@ -4,27 +4,64 @@ chai.use(chaiHttp);
 const login = require("../controllers/auth");
 const assert = chai.assert;
 const should = chai.should();
-const expect = chai.expect;
+const expect = require("chai").expect;
 const server = require("../app");
 let agent = chai.request.agent(server);
 //result
 
 describe("main suite", () => {
-  it("deberia debolver todos los contactos", async () => {
-    agent
-      .post("/auth/login")
-      .send({
-        email: "adminUser5@test.com",
-        password: "##StrongPassword05",
-      })
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        agent
+  describe("Caso 1 - GET contact", () => {
+    it("Comprueba que el status code sea correcto", async () => {
+      //consiguiendo la autorizacion
+      let get = await agent
+        .post("/auth/login")
+        .send({ email: "adminUser5@test.com", password: "##StrongPassword05" });
+
+      //consiguiendo los contactos
+      let getC = await agent
+        .get("/contacts")
+        .set("authorization", "Bearer " + get.body.accesToken);
+
+      expect(getC).to.have.status(200);
+    });
+
+    describe("Caso 2 - GET contact", () => {
+      it("Validacion tipo de dato", async () => {
+        //consiguiendo la autorizacion
+        let get = await agent.post("/auth/login").send({
+          email: "adminUser5@test.com",
+          password: "##StrongPassword05",
+        });
+
+        //consiguiendo los contactos
+        let getC = await agent
           .get("/contacts")
-          .set("authorization", "Bearer " + res.body.accesToken)
-          .then(function (res, err) {
-          });
+          .set("authorization", "Bearer " + get.body.accesToken);
+
+        getC.body.forEach((contact) => {
+          assert.typeOf(
+            contact.id,
+            "number",
+            "El tipo de dato esperado es un string"
+          );
+          assert.typeOf(
+            contact.name,
+            "string",
+            "El tipo de dato esperado es un string"
+          );
+          assert.typeOf(
+            contact.phone,
+            "number",
+            "El tipo de dato esperado es un number"
+          );
+          assert.typeOf(
+            contact.email,
+            "string",
+            "El tipo de dato esperado es un string"
+          );
+          //assert.typeOf(contact.message, "text");
+        });
       });
-    agent.close();
+    });
   });
 });
