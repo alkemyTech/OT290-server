6308321d0ed207751941562b721ca403e34f87b5
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-20 20:14:52 -0300

    fix: ajuste en datos retornados por modelo

--- models/user.js
+++ models/user.js
@@ -2,16 +2,21 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       User.belongsTo(models.Role, {
         as: "role",
         foreingKey: "roleId",
       });
+    }
+
+    toJSON() {
+      return {
+        ...this.get(),
+        password: undefined,
+        createdAt: undefined,
+        updatedAt: undefined,
+        deletedAt: undefined,
+      };
     }
   }
   User.init(
